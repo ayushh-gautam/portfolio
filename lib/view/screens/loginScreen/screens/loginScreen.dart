06034dbbee46f1c6ae0ce06211b1dafb2259abf9
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-04 23:10:43 +0545

    minor changes

--- lib/view/screens/loginScreen/screens/loginScreen.dart
+++ lib/view/screens/loginScreen/screens/loginScreen.dart
@@ -21,96 +21,98 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: Color(0xFF000000),
       body: LayoutBuilder(builder: ((context, Constraints) {
-        return SafeArea(
-          child: Container(
-            margin: EdgeInsets.only(
-                top: Constraints.maxHeight * 0.065,
-                left: Constraints.maxWidth * 0.034,
-                right: Constraints.maxWidth * 0.04),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MyText(
-                    text: 'Login',
-                    fontSize: 64,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600),
-                MyTextField(
-                  text: 'Username',
-                  controller: usernameCOntroller,
-                  left: Constraints.maxWidth * 0.032,
-                  right: Constraints.maxWidth * 0.032,
-                  top: Constraints.maxHeight * 0.135,
-                ),
-                MyTextField(
-                  text: 'Password',
-                  controller: usernameCOntroller,
-                  left: Constraints.maxWidth * 0.032,
-                  right: Constraints.maxWidth * 0.032,
-                  top: Constraints.maxHeight * 0.06,
-                ),
-                Padding(
-                    padding: EdgeInsets.only(
-                        top: Constraints.maxHeight * 0.035,
-                        left: Constraints.maxWidth * 0.5),
+        return SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              margin: EdgeInsets.only(
+                  top: Constraints.maxHeight * 0.065,
+                  left: Constraints.maxWidth * 0.034,
+                  right: Constraints.maxWidth * 0.04),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MyText(
+                      text: 'Login',
+                      fontSize: 64,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                  MyTextField(
+                    text: 'Username',
+                    controller: usernameCOntroller,
+                    left: Constraints.maxWidth * 0.032,
+                    right: Constraints.maxWidth * 0.032,
+                    top: Constraints.maxHeight * 0.135,
+                  ),
+                  MyTextField(
+                    text: 'Password',
+                    controller: usernameCOntroller,
+                    left: Constraints.maxWidth * 0.032,
+                    right: Constraints.maxWidth * 0.032,
+                    top: Constraints.maxHeight * 0.06,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(
+                          top: Constraints.maxHeight * 0.035,
+                          left: Constraints.maxWidth * 0.5),
+                      child: MyText(
+                        text: 'Forgot Password?',
+                        fontSize: 20,
+                        color: Color(0xff7e7e7e),
+                        fontWeight: FontWeight.normal,
+                      )),
+                  MyButton(
+                    ontap: () {},
+                    left: Constraints.maxWidth * 0.03,
+                    top: Constraints.maxHeight * 0.018,
+                    bottom: Constraints.maxHeight * 0.03,
+                    height: Constraints.maxHeight * 0.077,
+                    width: Constraints.maxWidth * 0.86,
+                  ),
+                  Center(
                     child: MyText(
-                      text: 'Forgot Password',
-                      fontSize: 20,
-                      color: Color(0xff7e7e7e),
                       fontWeight: FontWeight.normal,
-                    )),
-                MyButton(
-                  ontap: () {},
-                  left: Constraints.maxWidth * 0.03,
-                  top: Constraints.maxHeight * 0.018,
-                  bottom: Constraints.maxHeight * 0.03,
-                  height: Constraints.maxHeight * 0.077,
-                  width: Constraints.maxWidth * 0.86,
-                ),
-                Center(
-                  child: MyText(
-                    fontWeight: FontWeight.normal,
-                    text: 'or continue with',
-                    fontSize: 24,
-                    color: Color(0xff7e7e7e),
+                      text: 'or continue with',
+                      fontSize: 24,
+                      color: Color(0xff7e7e7e),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: Constraints.maxHeight * 0.03,
-                ),
-                Center(
-                  child: Image.asset(
-                    'lib/assets/icons/google.png',
-                    height: Constraints.maxHeight * 0.08,
+                  SizedBox(
+                    height: Constraints.maxHeight * 0.03,
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: Constraints.maxHeight * 0.03),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyText(
-                          text: 'Don\'t have an account?',
-                          color: Color(0xFF7e7e7e),
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushReplacement(context,
-                              MaterialPageRoute(builder: (context) {
-                            return SignUp();
-                          }));
-                        },
-                        child: MyText(
-                            text: ' Signup',
-                            color: Color(0xff03b44a),
+                  Center(
+                    child: Image.asset(
+                      'lib/assets/icons/google.png',
+                      height: Constraints.maxHeight * 0.08,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: Constraints.maxHeight * 0.03),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MyText(
+                            text: 'Don\'t have an account?',
+                            color: Color(0xFF7e7e7e),
                             fontSize: 20,
-                            fontWeight: FontWeight.w500),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                            fontWeight: FontWeight.normal),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return SignUp();
+                            }));
+                          },
+                          child: MyText(
+                              text: ' Signup',
+                              color: Color(0xff03b44a),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         );
--- lib/view/screens/loginScreen/screens/signupScreen.dart
+++ lib/view/screens/loginScreen/screens/signupScreen.dart
@@ -20,63 +20,67 @@ class _SignUpState extends State<SignUp> {
     return Scaffold(
       backgroundColor: Color(0xff000000),
       body: LayoutBuilder(builder: ((context, Constraints) {
-        return Container(
-            margin: EdgeInsets.only(
-                top: Constraints.maxHeight * 0.065,
-                left: Constraints.maxWidth * 0.034,
-                right: Constraints.maxWidth * 0.04),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MyText(
-                    text: 'Sign up',
-                    color: Colors.white,
-                    fontSize: 64,
-                    fontWeight: FontWeight.w600),
-                MyTextField(
-                    controller: emailController,
-                    text: 'Email',
-                    top: Constraints.maxHeight * 0.135,
-                    right: Constraints.maxWidth * 0.032,
-                    left: Constraints.maxWidth * 0.032),
-                MyTextField(
-                    controller: passController,
-                    text: 'Password ',
-                    top: Constraints.maxHeight * 0.05,
-                    right: Constraints.maxWidth * 0.032,
-                    left: Constraints.maxWidth * 0.032),
-                MyTextField(
-                    controller: confirmPassController,
-                    text: 'Confirm password ',
-                    top: Constraints.maxHeight * 0.05,
-                    right: Constraints.maxWidth * 0.032,
-                    left: Constraints.maxWidth * 0.032),
-                MyButton(
-                  bottom: Constraints.maxHeight * 0.05,
-                  top: Constraints.maxHeight * 0.045,
-                  left: Constraints.maxWidth * 0.03,
-                  height: Constraints.maxHeight * 0.077,
-                  width: Constraints.maxWidth * 0.86,
-                  ontap: null,
-                ),
-                Center(
-                  child: MyText(
-                      text: 'or continue with',
-                      color: Color(0xff7e7e7e),
-                      fontSize: 24,
-                      fontWeight: FontWeight.normal),
-                ),
-                SizedBox(
-                  height: Constraints.maxHeight * 0.03,
-                ),
-                Center(
-                  child: Image.asset(
-                    'lib/assets/icons/google.png',
-                    height: Constraints.maxHeight * 0.08,
-                  ),
-                )
-              ],
-            ));
+        return SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+                margin: EdgeInsets.only(
+                    top: Constraints.maxHeight * 0.065,
+                    left: Constraints.maxWidth * 0.034,
+                    right: Constraints.maxWidth * 0.04),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    MyText(
+                        text: 'Sign up',
+                        color: Colors.white,
+                        fontSize: 64,
+                        fontWeight: FontWeight.w600),
+                    MyTextField(
+                        controller: emailController,
+                        text: 'Email',
+                        top: Constraints.maxHeight * 0.135,
+                        right: Constraints.maxWidth * 0.032,
+                        left: Constraints.maxWidth * 0.032),
+                    MyTextField(
+                        controller: passController,
+                        text: 'Password ',
+                        top: Constraints.maxHeight * 0.05,
+                        right: Constraints.maxWidth * 0.032,
+                        left: Constraints.maxWidth * 0.032),
+                    MyTextField(
+                        controller: confirmPassController,
+                        text: 'Confirm password ',
+                        top: Constraints.maxHeight * 0.05,
+                        right: Constraints.maxWidth * 0.032,
+                        left: Constraints.maxWidth * 0.032),
+                    MyButton(
+                      bottom: Constraints.maxHeight * 0.05,
+                      top: Constraints.maxHeight * 0.045,
+                      left: Constraints.maxWidth * 0.03,
+                      height: Constraints.maxHeight * 0.077,
+                      width: Constraints.maxWidth * 0.86,
+                      ontap: null,
+                    ),
+                    Center(
+                      child: MyText(
+                          text: 'or continue with',
+                          color: Color(0xff7e7e7e),
+                          fontSize: 24,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    SizedBox(
+                      height: Constraints.maxHeight * 0.03,
+                    ),
+                    Center(
+                      child: Image.asset(
+                        'lib/assets/icons/google.png',
+                        height: Constraints.maxHeight * 0.08,
+                      ),
+                    )
+                  ],
+                )),
+          ),
+        );
       })),
     );
   }
