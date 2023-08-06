032ce66b648549ac16deefe0bcda90899a375afd
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-06 16:25:32 +0545

    home

--- lib/view/screens/Home/pages/homepage.dart
+++ lib/view/screens/Home/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +11,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          body: Column(
-        children: [
-          Container(),
-        ],
-      )),
-    );
+    return LayoutBuilder(builder: (context, Constraints) {
+      return SafeArea(
+        child: Scaffold(
+            body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: Constraints.maxHeight * 0.15,
+            ),
+            Center(
+              child: Container(
+                height: Constraints.maxHeight * 0.2,
+                decoration: BoxDecoration(
+                    color: Color(0xff434343),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(
+                      'lib/assets/images/Mask group (1).png',
+                      height: Constraints.maxHeight * 0.17,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      );
+    });
   }
 }
