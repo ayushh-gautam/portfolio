ca894f503ae53de2487b2b33a096fdf0767df7db
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-06 18:19:27 +0545

    timeline tiles

--- lib/view/screens/Home/elements/Timeline Tiles Elements/projectTimelineChild.dart
+++ lib/view/screens/Home/elements/Timeline Tiles Elements/projectTimelineChild.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/elements/myText.dart';
 
 class ProjectTimelineChild extends StatelessWidget {
   const ProjectTimelineChild({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 28.0, right: 28),
-      child: Container(
-        height: 50,
-        color: Colors.grey,
-        child: Center(
-          child: Text(
-            'gomu gomuno',
-            style: TextStyle(color: Colors.white),
+    return LayoutBuilder(
+      builder: (context, Constraints) {
+        return Padding(
+          padding: EdgeInsets.only(
+              left: Constraints.maxWidth * 0.05,
+              right: Constraints.maxWidth * 0.2),
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: Constraints.maxHeight * 0.55,
+              decoration: BoxDecoration(
+                  color: Color(0xff323232),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                  child: MyText(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                text: 'Project',
+              )),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
