37b4afd759d6135115c9662a2c2be776e1a78f81
ayushh-gautam <ayushgautam896@gmail.com>
2023-08-10 20:03:52 +0545

    minor change

--- lib/view/screens/loginScreen/screens/signupScreen.dart
+++ lib/view/screens/loginScreen/screens/signupScreen.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/elements/myText.dart';
