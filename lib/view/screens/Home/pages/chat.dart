d1237f8f4a97e9fe46ef38e31995311d52a83af8
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-11 13:22:27 +0545

    chatpage decoration

--- lib/view/screens/Home/pages/chat.dart
+++ lib/view/screens/Home/pages/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/elements/myText.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -11,7 +12,25 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('UserName')),
+      appBar: AppBar(
+        title: Text('UserName'),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        alignment: Alignment(0, 1),
+        child: Container(
+          height: 70,
+          width: 350,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30), color: Colors.white12),
+          alignment: Alignment(0, 0),
+          child: MyText(
+              text: 'text here',
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.normal),
+        ),
+      ),
     );
   }
 }
