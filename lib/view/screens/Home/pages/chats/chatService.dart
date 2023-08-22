486ad3621e89bf09c56ea71463e75d1eaf754f45
ayushh-gautam <ayushgautam896@gmail.com>
2023-08-22 17:40:00 +0545

    message model

--- lib/view/screens/Home/pages/chats/chatService.dart
+++ lib/view/screens/Home/pages/chats/chatService.dart
@@ -28,8 +28,29 @@ class ChatService extends ChangeNotifier {
 // construct chatroom id for current user and receiver id <sorted to ensure uniqueness>
     List<String> ids = [currentUserId, receiverId];
     ids.sort();
+    String chatRoomId = ids.join("_");
 // add new message to database
+
+    await _firestore
+        .collection('chatroom')
+        .doc(chatRoomId)
+        .collection("messages")
+        .add(newMessage.toMap());
   }
 
   // Get message
+
+  Stream<QuerySnapshot> getMessages(String userid, String otherUserId) {
+    //construct chatroom id
+
+    List<String> ids = [userid, otherUserId];
+    ids.sort();
+    String chatRoomId = ids.join("_");
+    return _firestore
+        .collection('chatroom')
+        .doc(chatRoomId)
+        .collection("messages")
+        .orderBy('timestamp', descending: false)
+        .snapshots();
+  }
 }
