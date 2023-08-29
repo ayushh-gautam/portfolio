a0ad6586a4063f6a97fba543a7bbb855a2610493
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-08-29 23:06:54 +0545

    ChangeProfilePic

--- lib/view/screens/loginScreen/Providers/profileProvider.dart
+++ lib/view/screens/loginScreen/Providers/profileProvider.dart
@@ -1,8 +1,1 @@
-import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:portfolio/view/screens/Home/database/updateprofile.dart';
-
-class changeProfileImage {}
