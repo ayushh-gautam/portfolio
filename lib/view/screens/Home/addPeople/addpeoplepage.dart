8e818fef2ed272151b89a1a921f2349adf931405
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-09-11 16:48:11 +0545

    search section start

--- lib/view/screens/Home/addPeople/addpeoplepage.dart
+++ lib/view/screens/Home/addPeople/addpeoplepage.dart
@@ -1,25 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/view/screens/loginScreen/elements/textField.dart';
 
-class AddPeoplePage extends StatelessWidget {
+class AddPeoplePage extends StatefulWidget {
   AddPeoplePage({super.key});
+
+  @override
+  State<AddPeoplePage> createState() => _AddPeoplePageState();
+}
+
+class _AddPeoplePageState extends State<AddPeoplePage> {
   TextEditingController usernameController = TextEditingController();
+  String username = '';
   @override
   Widget build(BuildContext context) {
+    // String emails = '';
     return Scaffold(
-      body: MyTextField(
-        obscureText: false,
-        controller: usernameController,
-        text: 'Search users',
-        left: MediaQuery.of(context).size.width * 0.032,
-        right: MediaQuery.of(context).size.width * 0.032,
-        top: MediaQuery.of(context).size.height * 0.1,
-        suffixIcon: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.search),
-          color: Colors.white,
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .where('email', isEqualTo: username)
+              .snapshots(),
+          builder: (context, snapshot) {
+            return Column(
+              children: [
+                MyTextField(
+                  obscureText: false,
+                  controller: usernameController,
+                  text: 'Search users',
+                  left: MediaQuery.of(context).size.width * 0.032,
+                  right: MediaQuery.of(context).size.width * 0.032,
+                  top: MediaQuery.of(context).size.height * 0.1,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        username = 'azzaya2060';
+                      });
+                    },
+                    icon: Icon(Icons.search),
+                    color: Colors.white,
+                  ),
+                ),
+
+                //
+              ],
+            );
+          }),
     );
   }
 }
