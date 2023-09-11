f2827f6464d1d66922f3b89d63cd9a61b3f2acd8
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-09-11 16:51:58 +0545

    search section mid

--- lib/view/screens/Home/addPeople/addpeoplepage.dart
+++ lib/view/screens/Home/addPeople/addpeoplepage.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/elements/myText.dart';
 import 'package:portfolio/view/screens/loginScreen/elements/textField.dart';
 
 class AddPeoplePage extends StatefulWidget {
@@ -16,36 +17,46 @@ class _AddPeoplePageState extends State<AddPeoplePage> {
   Widget build(BuildContext context) {
     // String emails = '';
     return Scaffold(
-      body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('users')
-              .where('email', isEqualTo: username)
-              .snapshots(),
-          builder: (context, snapshot) {
-            return Column(
-              children: [
-                MyTextField(
-                  obscureText: false,
-                  controller: usernameController,
-                  text: 'Search users',
-                  left: MediaQuery.of(context).size.width * 0.032,
-                  right: MediaQuery.of(context).size.width * 0.032,
-                  top: MediaQuery.of(context).size.height * 0.1,
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        username = 'azzaya2060';
-                      });
-                    },
-                    icon: Icon(Icons.search),
-                    color: Colors.white,
-                  ),
-                ),
-
-                //
-              ],
-            );
-          }),
+      body: Column(
+        children: [
+          MyTextField(
+            obscureText: false,
+            controller: usernameController,
+            text: 'Search users',
+            left: MediaQuery.of(context).size.width * 0.032,
+            right: MediaQuery.of(context).size.width * 0.032,
+            top: MediaQuery.of(context).size.height * 0.1,
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  username = 'azzaya2060';
+                });
+              },
+              icon: Icon(Icons.search),
+              color: Colors.white,
+            ),
+          ),
+          (username != '')
+              ? StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .where('email', isEqualTo: username)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    return Column(
+                      children: [
+                        MyText(
+                            text: '00000000000',
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal)
+                        //
+                      ],
+                    );
+                  })
+              : Container(),
+        ],
+      ),
     );
   }
 }
